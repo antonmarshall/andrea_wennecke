e5bf934dffeffddd57e64218a1b6529d2d8a5a33
antonmarshall <anton.marshall03@gmail.com>
2025-04-15 17:37:40 +0200

    Update vite.config.ts
    test. maybe too little

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,9 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
-
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  // Set base conditionally depending on the environment
-  base: process.env.NODE_ENV === 'production' && !process.env.LOVABLE ? "/andrea_wennecke/" : "/",
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+export default defineConfig({
+  // Der base-Pfad gibt an, unter welchem Unterverzeichnis deine App gehostet wird.
+  // In deinem Fall: https://<username>.github.io/andrea-wennecke/
+  base: '/andrea-wennecke/',
+  plugins: [react()]
+});
